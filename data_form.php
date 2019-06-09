1e6de8ffbd5a00b8c417f6067b41d46e58a0c785
jishant07 <jishanta@gmail.com>
2019-06-10 01:20:25 +0530

    Form Added

--- data_form.php
+++ data_form.php
@@ -13,18 +13,20 @@
 <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
 <!-- Plugin CSS -->
 <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="https://buddycss-cc55.kxcdn.com/downloads/1.0.7/css/buddy.plugins.min.css">
 <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="https://buddycss-cc55.kxcdn.com/downloads/1.0.7/css/buddy.min.css">
 <!-- Theme CSS - Includes Bootstrap -->
 <link href="css/creative.min.css" rel="stylesheet">
 <style type="text/css">
-	html 
+	*
 	{
-	  font-family: sans-serif;
-	  line-height: 1.15;
-	  -webkit-text-size-adjust: 100%;
-	  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+		font-size: 15px;
+		text-align: center;
+		margin: auto;
+	}
+	input,select
+	{
+		width:100%;
+		padding:10px;
 	}
 </style>
 </head>
@@ -35,24 +37,189 @@
 			<h1>Add the details here.</h1>
 		</div>
 	</div>
-	<div class="container" style="width:75%;">
+	<div class="container">
 		<br>
-		<form style="border:solid orange 2px;">
+		<form>
+			<div class="row">
+				<div class="col-lg-3"><input type="text" name="name" placeholder="Name"></div>
+				<div class="col-lg-3"><input type="text" name="file_number" placeholder="File Number"></div>
+				<div class="col-lg-3"><input type="text" name="reg_number" placeholder="Register Number"></div>
+				<div class="col-lg-3"><input type="number" name="aadhar_number" placeholder="Aadhar Number"></div>
+			</div>
 			<br>
 			<div class="row">
-				<div class="col-lg-6">
-					<div class="input-animation">
-						<label form="input">Name</label>
-						<input type="text" id="input" name="name"/>
-					</div>
+				<div class="col-lg-3">
+					<textarea style="width:100%;" placeholder="Permanent Address"></textarea>
 				</div>
-				<div class="col-lg-6">
-					<div class="input-animation">
-						<label form="input">File Number</label>
-						<input type="text" id="input" name="file_number"/>
-					</div>
+				<div class="col-lg-3">
+					<textarea style="width:100%;" placeholder="Local Address"></textarea>
+				</div>
+				<div class="col-lg-3">
+					<input type="text" name="tehsil" placeholder="Tehsil">
+				</div>
+				<div class="col-lg-3"><input type="text" name="taluka" placeholder="Taluka">
 				</div>
 			</div>
+			<br>
+			<div class="row">
+				<div class="col-lg-3"><input type="text" name="village" placeholder="village"></div>
+				<div class="col-lg-3">
+					<input type="number" name="mob_num" placeholder="Mobile Number">
+				</div>
+				<div class="col-lg-3"><input type="number" name="age" placeholder="Age">
+				</div>
+				<div class="col-lg-3"><input type="date" name="date_of_birth" placeholder="Date of Birth"></div>
+			</div>
+			<br>
+			<div class="row">
+				<div class="col-lg-3">
+					<input type="number" name="height" placeholder="Height">
+				</div>
+				<div class="col-lg-3">
+					<input type="number" name="weight" placeholder="Weight">
+				</div>
+				<div class="col-lg-3"><input type="number" name="chest" placeholder="Chest"></div>
+				<div class="col-lg-3"><input type="text" name="comp" placeholder="Complexion"></div>
+			</div>
+			<br>
+			<div class="row">
+				<div class="col-lg-3">
+					<select name="eye_color">
+						<option selected disabled>Select an Eye Colour</option>
+						<option>Balck</option>
+						<option>Brown</option>
+						<option>Green</option>
+						<option>Hazel</option>
+						<option>Gold</option>
+						<option>Other</option>
+					</select>
+				</div>
+				<div class="col-lg-3">
+					<select name="nose_type">
+						<option selected disabled>Select Nose Type</option>
+						<option>Turned- Up</option>
+						<option>Roman</option>
+						<option>Bumpy</option>
+						<option>Snub</option>
+						<option>Hawk</option>
+						<option>Other</option>
+					</select>
+				</div>
+				<div class="col-lg-3">
+					<input type="text" name="hair" placeholder="Hair">
+				</div>
+				<div class="col-lg-3">
+					<input type="number" name="teeth" placeholder="No of Teeth">
+				</div>
+			</div>
+			<br>
+			<div class="row">
+				<div class="col-lg-3">
+					<select name="lip_type">
+						<option selected disabled>Select lips type</option>
+						<option>Heart Shaped</option>
+						<option>Thin</option>
+						<option>Goldilocks</option>
+						<option>Wide</option>
+						<option>Plump</option>
+						<option>Other</option>
+					</select>
+				</div>
+				<div class="col-lg-3">
+					<textarea style="width:100%" placeholder="General Condition" name="general_condition"></textarea>
+				</div>
+				<div class="col-lg-3">
+					<input type="text" name="religion" placeholder="Religion">
+				</div>
+				<div class="col-lg-3">
+					<input type="text" name="caste" placeholder="Caste">
+				</div>
+			</div>
+			<br>
+			<a id="add_language" style="text-align:left;float:left;">Add Language</a>
+			<div class="row">
+				<div class="col-lg-3">
+					<input type="text" name="other_languages[]" placeholder="Other Languages">
+				</div>
+				<div class="col-lg-3">
+					<input type="text" name="mother_tongue" placeholder="Mother Tongue">
+				</div>
+				<div class="col-lg-3">
+					<input type="text" name="socio_economic_class" placeholder="Socio Economic Class">
+				</div>
+				<div class="col-lg-3">
+					<select name="lit_level">
+						<option>High</option>
+						<option>Medium</option>
+						<option>Low</option>
+					</select>
+				</div>
+			</div>
+			<br>
+			<div class="row">
+				<div class="col-lg-3">
+					<input type="text" name="prev_school" placeholder="Previous School">
+				</div>
+				<div class="col-lg-3">
+					<input type="text" name="child_court" placeholder="Children Court">
+				</div>
+				<div class="col-lg-3">
+					<input type="text" name="case_no" placeholder="Case Number">
+				</div>
+				<div class="col-lg-3">
+					<input type="text" name="case_handler" placeholder="Case Handler">
+				</div>
+			</div>
+			<br>
+			<div class="row">
+				<div class="col-lg-3">
+					<input type="text" name="section_referral" placeholder="Section Referral">
+				</div>
+				<div class="col-lg-3">
+					<select name="status">
+						<option>CCL</option>
+						<option>Child Labour</option>
+						<option>Racket</option>
+						<option>Others</option>
+					</select>
+				</div>
+				<div class="col-lg-3">
+					<input type="date" name="date_of_custody" placeholder="Date of Custody">
+				</div>
+				<div class="col-lg-3">
+					<input type="date" name="date_of_admission" placeholder="Date of Addmission">
+				</div>
+			</div>
+			<br>
+			<div class="row">
+				<div class="col-lg-3">
+					<select name="is_repeater">
+						<option>Yes</option>
+						<option>No</option>
+						<option>Don't know</option>
+					</select>
+				</div>
+				<div class="col-lg-3">
+					<input type="date" name="date_of_discharge" placeholder="Date of Discharge">
+				</div>
+				<div class="col-lg-3">
+					<input type="text" name="prev_institution" placeholder="Previous Institution">
+				</div>
+				<div class="col-lg-3">
+					
+				</div>
+			</div>
+			<br>
+			<button id="add_medical_row"></button>
+			<div class="row">
+				<div class="col-lg-4"><h3>Medical</h3>
+				<br><input type="text" name="medical[]"></div>
+				<div class="col-lg-4"><h3>Psychological</h3>
+				<br><input type="text" name="psychological[]"></div>
+				<div class="col-lg-4"><h3>Psychiatric</h3>
+				<br><input type="text" name="psychiatric[]"></div>
+			</div>
+			<br>
 		</form>
 	</div>
 </body>
@@ -63,5 +230,7 @@
 <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
 <!-- Custom scripts for this template -->
 <script src="js/creative.min.js"></script>
-<script type="text/javascript" src="https://buddycss-cc55.kxcdn.com/downloads/1.0.7/js/buddy.min.js"></script>
+<script type="text/javascript">
+	
+</script>
 </html>
