51e90c2dc7cb23b3d438c6e4e9ab0c97d48112a0
jishant07 <jishanta@gmail.com>
2019-06-23 19:03:46 +0530

    Minor tweaks

--- retrive_data.php
+++ retrive_data.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Data Retrival Page</title>
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="jumbotron">
+			<h1>Data Retrival</h1>
+		</div>
 <?php 
 	require("./database_credentials.php");
 	$sql = "select * from children_data";
@@ -8,19 +19,25 @@
 		{
         	$test = $row["added_data"];
         	$res = json_decode($test,true);
-        	if($res['name'] == "")
-        	{
-        		echo "Name not present";
-        	}
-        	else
-        	{
-        		/*Example of data retrival*/
-        		echo $res['name'];
-        	}
-        }
+?>
+<?php 
+			if ($res['name'] == "") 
+			{
+				echo "<h3> Name Not Present </h3>";
+			}
+			else 
+			{
+				echo "<h3>".$res['name']."</h3>";
+			}	 
+?>
+</div>
+</body>
+</html>
+<?php 
+		}
 	}
 	else
 	{
-		echo "Data is missing";
+		echo "No data present";
 	}
- ?>
+?>
